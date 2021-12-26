6ebd033389b1ab8e8e4eb196e477448ea9d32091
Akahibe Kingsley <divine10646@gmail.com>
2021-12-26 23:36:47 +0100

    Changed tradingview widget

--- 01-Dark-Theme/js/custom.js
+++ 01-Dark-Theme/js/custom.js
@@ -404,10 +404,19 @@ const widgetContainer = document.querySelector("#tradingview-widget");
 let widgetCode = `
 <!-- TradingView Widget BEGIN -->
 <div class="tradingview-widget-container">
-  <div id="tradingview_ea1d4"></div>
-  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/symbols/NASDAQ-AAPL/" rel="noopener" target="_blank"><span class="blue-text">AAPL Chart</span></a> by TradingView</div>
-  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-  
+  <div class="tradingview-widget-container__widget"></div>
+  <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/markets/cryptocurrencies/prices-all/" rel="noopener" target="_blank"><span class="blue-text">Cryptocurrency Markets</span></a> by TradingView</div>
+  <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-screener.js" async>
+  {
+  "width": 1000,
+  "height": 490,
+  "defaultColumn": "overview",
+  "screener_type": "crypto_mkt",
+  "displayCurrency": "USD",
+  "colorTheme": "light",
+  "locale": "en"
+}
+  </script>
 </div>
 <!-- TradingView Widget END -->
 `;
