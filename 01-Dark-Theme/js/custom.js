c00102e463c7bd254cfa5119901e3b2b6375a6ce
Akahibe Kingsley <divine10646@gmail.com>
2021-12-29 13:57:21 +0100

    chaned chart background to transaparent

--- 01-Dark-Theme/js/custom.js
+++ 01-Dark-Theme/js/custom.js
@@ -408,13 +408,14 @@ let widgetCode = `
   <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/markets/cryptocurrencies/prices-all/" rel="noopener" target="_blank"><span class="blue-text">Cryptocurrency Markets</span></a> by TradingView</div>
   <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-screener.js" async>
   {
-  "width": "100%",
-  "height": 590,
+  "width": 1000,
+  "height": 490,
   "defaultColumn": "overview",
   "screener_type": "crypto_mkt",
   "displayCurrency": "USD",
   "colorTheme": "light",
-  "locale": "en"
+  "locale": "en",
+  "isTransparent": true
 }
   </script>
 </div>
